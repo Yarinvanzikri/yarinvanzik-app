112773b72bb25d456159d0052c2bbe4a9be21298
YarinVanZikri <yarinvanzikri@gmail.com>
2021-07-24 23:15:13 +0300

    modified files for avatar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Avatar from "./Avatar/Avatar";
+let link = 'https://cdn.hiconsumption.com/wp-content/uploads/2020/04/Best-Fixed-Gear-Bicvcles-00-Hero.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Avatar address={link} />
     </div>
   );
 }
---
+++ src/Avatar/Avatar.js
@@ -0,0 +1,4 @@
+import './Avatar.css';
+export default function Avatar(props) {
+    return <img className="Avatar" src={props.address}></img>
+};
